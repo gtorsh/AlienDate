872ae50d3ef574dfc318a617ec0e0d96e6f38d7c
Adam <aboulding17@gmail.com>
2016-05-17 11:13:36 -0600

    Update with Basic Collisions!

--- HAWTALIENS/Assets/Scripts/Movement.cs
+++ HAWTALIENS/Assets/Scripts/Movement.cs
@@ -3,21 +3,48 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	public float speed;
+	public float movespeed;
+
+	private Animator anim;
+	private Rigidbody2D myRigidbody;
+
+	private bool playerMoving;
+	private Vector2 lastMove;
+
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		myRigidbody = GetComponent<Rigidbody2D> ();
+	}
 
 	void Update () {
-		if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (Vector2.right * speed);
+
+		playerMoving = false;
+
+		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
+			playerMoving = true;
+			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+			myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movespeed, myRigidbody.velocity.y);
 		} 
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate (Vector2.left * speed);
+		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
+			playerMoving = true;
+			lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * movespeed);
 		} 
-		if (Input.GetKey (KeyCode.W)) {
-			transform.Translate (Vector2.up * speed);
-		} 
-		if (Input.GetKey (KeyCode.S)) {
-			transform.Translate (Vector2.down * speed);
+
+		if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5) {
+			myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
 		}
+		
+		if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5) {
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+		}
+
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Vertical"));
+		anim.SetBool("PlayerMoving", playerMoving);
+		anim.SetFloat("LastMoveX", lastMove.x);
+		anim.SetFloat("LastMoveY", lastMove.y);
 	
 	}
 }
