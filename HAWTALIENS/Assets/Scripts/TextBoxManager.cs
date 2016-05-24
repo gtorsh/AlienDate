30e10e657054ae5eec053dee82c10467705abbae
Adam <aboulding17@gmail.com>
2016-05-24 00:54:33 -0600

    Dialogue Bitcheeees

--- HAWTALIENS/Assets/Scripts/TextBoxManager.cs
+++ HAWTALIENS/Assets/Scripts/TextBoxManager.cs
@@ -13,6 +13,7 @@ public class TextBoxManager : MonoBehaviour {
 	public string Conversation;
 	public string Character; 
 	public string[] textLines;
+	private static string myFile;
 
 	public int currentLine;
 	public int endAtLine;
@@ -24,35 +25,39 @@ public class TextBoxManager : MonoBehaviour {
 		currentLine = 0;
 		player = FindObjectOfType<Movement> ();
 
+		if (Character == null && Conversation == null) {
+			TextAsset BaseCamp = (TextAsset)AssetDatabase.LoadAssetAtPath ("Assets/Data/BaseCamp.txt", typeof(TextAsset));  
+			textLines = (BaseCamp.text.Split ('\n')); 
+		} else {
+			myFile = "Assets/Data/Dialogue_";
+			myFile += Character + "_"; 
+			myFile += Conversation;
+			myFile += ".txt";
+
+			Debug.Log (myFile);
+
+			TextAsset goodText = (TextAsset)AssetDatabase.LoadAssetAtPath (myFile, typeof(TextAsset));  
+			textLines = (goodText.text.Split ('\n')); 
+		}
 		if (endAtLine == 0) {
 			endAtLine = textLines.Length - 1;
 		}
-
-		if (Character == null && Conversation == null) {
-			TextAsset BaseCamp = (TextAsset)AssetDatabase.LoadAssetAtPath("Assets/Data/BaseCamp.txt", typeof(TextAsset));  
-			textLines = (BaseCamp.text.Split ('\n')); 
-
-			string myFile = Character; 
-			myFile += Conversation;
-			myFile += ".txt";
-			Debug.Log (myFile);
-		} 
-		
 	}
 
 
 	// Update is called once per frame
 	void Update () {
 
-			theText.text = textLines[currentLine];
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			currentLine += 1;
+		}
 
-			if(Input.GetKeyDown(KeyCode.Return)) {
-				currentLine += 1;
-			}
-
-			if (currentLine > endAtLine) {
-				textBox.SetActive(false);
-			}
+		if (currentLine <= endAtLine) {
+			theText.text = textLines [currentLine];
+		} else {
+			textBox.SetActive(false);
+			theText.text = null;
+		}
 	}
 
 }
